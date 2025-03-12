69b4f9d7279f96e99a51a156b25aa03296868301
bharadwajacss <153656215+bharadwajacss@users.noreply.github.com>
2025-03-12 18:06:04 +0000

    " Linked List test added"

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client"; // Updated import statement
 import "../node_modules/bootstrap/dist/css/bootstrap.css";
 import BinaryTreeComponent from "./components/binarytree.jsx";
+import App from "./App.js";
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
